451f8ff0e4fdfc39724c5b7d3247bab90edec1cf
leoscarton <lfscarton@gmail.com>
2026-09-18 01:01:51 -0300

    Began FruitCounter functions

--- backend/vision/fruit_detector.py
+++ backend/vision/fruit_detector.py
@@ -1,9 +1,21 @@
 import cv2
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import List, Tuple
 
 @dataclass
 class FruitCounter:
     name: str
     count: int
-    position_on_screen: List[Tuple[float]] # Subject to change
+    #is_on_screen: bool = False
+    position_on_screen: List[Tuple[float, float]] = field(default_factory=list) # Subject to change
+
+    def __post_init__(self):
+        for p in self.position_on_screen:
+            if len(p) != 2:
+                raise ValueError(f"Each tuple must have exactly 2 elements, {p} has {len(p)}")
+
+        if self.count < 0:
+            raise ValueError("Fruit count cannot be negative")
+
+    def fruit_detected(self):
+        return (self.count > 0)
