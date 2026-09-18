bbf81f8ce99aa1723abd2f9443951442f0105e0d
leoscarton <lfscarton@gmail.com>
2026-09-18 01:12:45 -0300

    Added functions to update fruit count and add a fruit position on screen in FruitCounter

--- backend/vision/fruit_detector.py
+++ backend/vision/fruit_detector.py
@@ -6,16 +6,31 @@ from typing import List, Tuple
 class FruitCounter:
     name: str
     count: int
-    #is_on_screen: bool = False
     position_on_screen: List[Tuple[float, float]] = field(default_factory=list) # Subject to change
 
     def __post_init__(self):
         for p in self.position_on_screen:
             if len(p) != 2:
                 raise ValueError(f"Each tuple must have exactly 2 elements, {p} has {len(p)}")
+            elif (p[0] < 0.0) or (p[1] < 0.0):
+                raise ValueError(f"Screen coordinates cannot be negative")
 
         if self.count < 0:
             raise ValueError("Fruit count cannot be negative")
 
     def fruit_detected(self):
         return (self.count > 0)
+
+    def update_count(self, new_count):
+        if new_count < 0:
+            raise ValueError("Fruit count cannot be negative")
+        else:
+            self.count = new_count
+
+    def add_fruit_position(self, fruit_pos:tuple):
+        if len(fruit_pos) != 2:
+            raise ValueError(f"Each tuple must have exactly 2 elements, {fruit_pos} has {len(fruit_pos)}")
+        elif fruit_pos[0] < 0.0 or fruit_pos[1] < 0.0:
+            raise ValueError(f"Screen coordinates cannot be negative")
+        else:
+            self.position_on_screen.append(fruit_pos)
